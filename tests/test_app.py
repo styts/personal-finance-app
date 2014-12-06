4d8a2ebdb502679a7b8b0b67781213b106d62150
Kirill Stytsenko <kirill@styts.com>
2014-12-06 16:35:32 +0100

    File upload form on the root view

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,4 +3,8 @@ from flask import url_for
 
 def test_app(client):
     # we can access the root view
-    assert client.get(url_for('root')).status_code == 200
+    response = client.get(url_for('root'))
+    assert response.status_code == 200
+
+    # it contains a file submission form
+    assert '<input type="file">' in response.get_data()
